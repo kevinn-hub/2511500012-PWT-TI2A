3a5a8187332a2b456da27e262d594a879494b48b
kevinn-hub <2511500012@mahasiswa.atmaluhur.ac.id>
2026-04-22 22:40:15 +0700

    first commit

--- Pertemuan-03/page/ganti_password.php
+++ Pertemuan-03/page/ganti_password.php
@@ -9,81 +9,69 @@
 </div>
 
 <?php
-if (isset($_POST['tambah'])) {
+if(isset($_POST['tambah'])){
+
     $pw_lama = $_POST['password_lama'];
     $pw_baru = $_POST['password_baru'];
     $konfirmasi = $_POST['konfirmasi'];
+
     $Username = $_SESSION['Username'];
 
-    // Ambil password lama dari database
-    $query = mysqli_query($koneksi, "SELECT Password FROM tbl_users WHERE Username='$Username'")or die(mysqli_error($koneksi));
-    $cek = mysqli_fetch_array($query);
-
-    if ($cek) {
-        if ($pw_lama == $cek['Password']) {
-            if ($pw_baru == $konfirmasi) {
-                // Update password
-                $update = mysqli_query($koneksi, "UPDATE tbl_users SET Password='$pw_baru' WHERE  Password='$pw_lama' ")or die(mysqli_error($koneksi));
-                if ($update) {
-                    echo '<div class="alert alert-success alert-dismissible">
-                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                        <h5><i class="icon fas fa-check"></i> Sukses</h5>
-                        Password berhasil diubah.
-                    </div>';
-                } else {
-                    echo '<div class="alert alert-danger alert-dismissible">
-                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                        <h5><i class="icon fas fa-ban"></i> Gagal</h5>
-                        Gagal mengubah password.
-                    </div>';
-                }
-            } else {
-                echo '<div class="alert alert-warning alert-dismissible">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                    <h5><i class="icon fas fa-exclamation-triangle"></i> Peringatan</h5>
-                    Password baru dan konfirmasi tidak cocok.
-                </div>';
-            }
-        } else {
-            echo '<div class="alert alert-danger alert-dismissible">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                <h5><i class="icon fas fa-ban"></i> Gagal</h5>
-                Password lama salah.
-            </div>';
-        }
+        if($pw_baru != $konfirmasi){
+        echo '<div class="alert alert-warning alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h5><i class="icon fas fa-exclamation-triangle"></i> Peringatan</h5>
+        <h4>Password baru dan konfirmasi tidak sama</h4></div>';
     } else {
-        echo '<div class="alert alert-danger alert-dismissible">
+        // ambil data user
+        $cek = mysqli_fetch_array(mysqli_query($koneksi,"SELECT * FROM tbl_users WHERE Username='$Username'"));
+        // cek hasil query
+        if($cek){
+            $update = mysqli_query($koneksi,"UPDATE tbl_users SET Password='$pw_baru' WHERE Password='$pw_lama' AND Username='$Username'");
+            $hasil = mysqli_affected_rows($koneksi);
+            if($update && $hasil >0){
+                echo '<div class="alert alert-success alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h5><i class="icon fas fa-check"></i> Sukses</h5>
+                <h4>Password berhasil diubah</h4></div>';
+
+            } else {
+            echo '<div class="alert alert-danger alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h5><i class="icon fas fa-ban"></i> Gagal</h5>
-            User tidak ditemukan.
-        </div>';
+            <h5><i class="icon fas fa-ban"></i> Error</h5>
+            <h4>Password lama salah</h4></div>';
+        }
     }
+}
 }
 ?>
 
 <section class="content">
     <div class="container-fluid">
         <div class="card">
-            <div class="card-body">
-                <div class="card-body p-2">
-                    <form method="POST" action="">
-                        <div class="form-group">
-                            <label for="password_lama">Password Lama</label>
-                            <input type="password" name="password_lama" id="password_lama" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="password_baru">Password Baru</label>
-                            <input type="password" name="password_baru" id="password_baru" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="konfirmasi">Konfirmasi Password Baru</label>
-                            <input type="password" name="konfirmasi" id="konfirmasi" class="form-control" required>
-                        </div>
-                        <div class="card-footer">
-                            <input type="submit" class="btn btn-primary" name="tambah" value="Ganti Password">
-                        </div>
-                    </form>
-                </div>
+            <div class="card-body p-2">
+                <form method="POST" action="">
+                    
+                    <div class="form-group">
+                        <label for="pl">Password Lama</label>
+                        <input type="password" name="password_lama" id="password_lama" class="form-control" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="pb">Password Baru</label>
+                        <input type="password" name="password_baru" id="password_baru" class="form-control" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="konfirmasi">Konfirmasi Password Baru</label>
+                        <input type="password" name="konfirmasi" id="konfirmasi" class="form-control" required>
+                    </div>
+
+                    <div class="card-footer">
+                        <input type="submit" class="btn btn-primary" name="tambah" value="Ganti Password">
+                    </div>
+
+                </form>
             </div>
         </div>
     </div>
