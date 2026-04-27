8364a2cd39d1ef5f0e299112edc87cf74350bc9c
kevinn-hub <2511500012@mahasiswa.atmaluhur.ac.id>
2026-04-27 21:05:06 +0700

    first commit

--- Pertemuan-03/page/ganti_password.php
+++ Pertemuan-03/page/ganti_password.php
@@ -10,38 +10,40 @@
 
 <?php
 if(isset($_POST['tambah'])){
-
-    $pw_lama = $_POST['password_lama'];
-    $pw_baru = $_POST['password_baru'];
+    $pw_lama    = $_POST['password_lama'];
+    $pw_baru    = $_POST['password_baru'];
+    $Username   = $_SESSION['Username'];
     $konfirmasi = $_POST['konfirmasi'];
-    $Username = $_SESSION['Username'];
 
-        if($pw_baru != $konfirmasi){
+      if ($pw_baru != $konfirmasi) {
         echo '<div class="alert alert-warning alert-dismissible">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
         <h5><i class="icon fas fa-exclamation-triangle"></i> Peringatan</h5>
         <h4>Password baru dan konfirmasi tidak sama</h4></div>';
-    } else {
-        // ambil data user
-        $cek = mysqli_fetch_array(mysqli_query($koneksi,"SELECT * FROM tbl_users WHERE Username='$Username'"));
-        // cek hasil query
-        if($cek){
-            $update = mysqli_query($koneksi,"UPDATE tbl_users SET Password='$pw_baru' WHERE Password='$pw_lama' AND Username='$Username'");
-            $hasil = mysqli_affected_rows($koneksi);
-            if($update && $hasil >0){
-                echo '<div class="alert alert-success alert-dismissible">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                <h5><i class="icon fas fa-check"></i> Sukses</h5>
-                <h4>Password berhasil diubah</h4></div>';
-                echo '<meta http-equiv="refresh" content="1;url=index.php">';
-            } else {
-            echo '<div class="alert alert-danger alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h5><i class="icon fas fa-ban"></i> Error</h5>
-            <h4>Password lama salah</h4></div>';
+        } else {
+
+        $cek = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM tbl_users WHERE Username = '$Username'"));
+
+      if($cek){
+
+        $update = mysqli_query($koneksi, "UPDATE tbl_users SET Password = '$pw_baru' WHERE Password = '$pw_lama' AND Username = '$Username'");
+
+            if($update){
+                if (mysqli_affected_rows($koneksi) > 0) {
+                     echo'<div class="alert alert-success alert-dismissible">
+                          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                          <h5><i class="icon fas fa-check"></i> Sukses</h5>
+                          <h4>Password berhasil diubah</h4></div>';
+                     echo'<meta http-equiv="refresh" content="1;url=index.php">';
+                } else {
+                     echo'<div class="alert alert-danger alert-dismissible">
+                          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                          <h5><i class="icon fas fa-ban"></i> Error</h5>
+                          <h4>Password lama salah</h4></div>';
+                }
+            }
         }
     }
-}
 }
 ?>
 
@@ -61,7 +63,7 @@ if(isset($_POST['tambah'])){
                         <input type="password" name="password_baru" id="password_baru" class="form-control" required>
                     </div>
 
-                    <div class="form-group">
+                   <div class="form-group">
                         <label for="konfirmasi">Konfirmasi Password Baru</label>
                         <input type="password" name="konfirmasi" id="konfirmasi" class="form-control" required>
                     </div>
@@ -75,4 +77,3 @@ if(isset($_POST['tambah'])){
         </div>
     </div>
 </section>
-
